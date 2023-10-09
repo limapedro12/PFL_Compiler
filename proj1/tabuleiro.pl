b67f0cb2c2964dfee1fb2ab5569c15227e17a3dd
up202108806 <up202108806@edu.fe.up.pt>
2023-10-09 16:57:57 +0100

    Temos Movimento Sexy!

--- proj1/tabuleiro.pl
+++ proj1/tabuleiro.pl
@@ -1,9 +1,25 @@
-play :- write('Hello World').
+:-use_module(library(lists)).
 
 inicializar_tabuleiro(T) :- T = [[0,   0, 0,  0, 0,   0, 0],
                                  [0, gp1, 0, rp, 0, gp2, 0],
-                                 [0, 0, 0, 0, 0, 0, 0],
-                                 [0, 0, 0, 0, 0, 0, 0],
-                                 [0, 0, 0, 0, 0, 0, 0],
+                                 [0,   0, 0,  0, 0,   0, 0],
+                                 [0,   0, 0,  0, 0,   0, 0],
+                                 [0,   0, 0,  0, 0,   0, 0],
                                  [0, gb1, 0, rb, 0, gb2, 0],
-                                 [0, 0, 0, 0, 0, 0, 0]].
+                                 [0,   0, 0,  0, 0,   0, 0]].
+
+substituir(Pos, List, NewElem, ListFinal, OldElem) :- nth1(Pos, List, OldElem, _R),
+                                                      nth1(Pos, ListFinal, NewElem, _R).
+
+mover_peca(T, Xi, Yi, Xf, Yf, T2) :-   nth1(Yi, T, LinhaInicial),
+                                        substituir(Xi, LinhaInicial, 0, LinhaInicialTemp, Peca),
+                                        substituir(Yi, T, LinhaInicialTemp, T3, _X1),
+                                        nth1(Yf, T3, LinhaFinal),
+                                        substituir(Xf, LinhaFinal, Peca, LinhaFinalTemp, _X2),
+                                        substituir(Yf, T3, LinhaFinalTemp, T4, _X3),
+                                        T2 = T4.
+                                     
+
+play :- write('1 - jogar | 0 - sair'),nl,
+        read(_X),nl,
+        write(_X).
